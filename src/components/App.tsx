17f47ff3b7b9d7299c13ba3f6c5481b12d49dede
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-02 19:50:03 +0200

    parsing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,6 @@
+import Ajv from 'ajv';
+import standaloneCode from 'ajv/dist/standalone';
+import jsyaml from 'js-yaml';
 import React, { useState } from 'react';
 
 import useLocalStorage from '../hooks/useLocalStorage';
@@ -8,23 +11,51 @@ function App(): JSX.Element {
   const [click, setClick] = useState(true);
   //tutaj prypisanie do zmiennej !!!!!!!!!!!!!
   const man = yaml;
+  function parseYamltoJSON(text: string) {
+    // Parsing string to JSON
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const doc = jsyaml.load(text);
+    // eslint-disable-next-line no-console
+    console.log(text);
+    // eslint-disable-next-line no-console
+    console.log(doc);
+    // eslint-disable-next-line no-console
+    console.log(typeof doc);
+    // const ret = JSON.parse(doc); nie wiem czy tak, może(?)
+    // ^^^ - showing type of parsed string
+    return doc;
+  }
+
+  function validate(schema: Record<string, unknown>) {
+    const ajv = new Ajv({ code: { source: true } });
+
+    const valid = ajv.compile(schema);
+    // eslint-disable-next-line no-console
+    console.log(typeof valid);
+    const moduleCode = standaloneCode(ajv, valid);
+    // eslint-disable-next-line no-console
+    console.log(typeof moduleCode);
+  }
   function handleClickEvent(event: any) {
     setClick(!click);
     if (click) {
       // eslint-disable-next-line no-console
       console.log(man);
-      const obj = JSON.parse(man);
+      // Tutaj chyba coś wywala, bo nie może wykonać JSON.parse (idk)
+      // const obj = JSON.parse(man);
       // eslint-disable-next-line no-console
-      console.log(obj);
+      // console.log(obj);
     }
     setClick(!click);
+    const x = parseYamltoJSON(man);
+    validate(x);
   }
   return (
     <>
       <div className="text-editor">
         <Editor value={yaml} onChange={setYaml} press={click} />
       </div>
-      <div className="result">{click ? yaml : 'D'}</div>
+      <div className="result">{click ? yaml : ''}</div>
       <button className="PRESSME" onClick={handleClickEvent}>
         KONWERTUJ
       </button>
