9bda20f0a860162f27a687a5fbc29d75edc1d3f6
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-02 16:53:09 +0200

    types in editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,11 +3,13 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/yaml/yaml';
 import { Controlled } from 'react-codemirror2';
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
-export default function Editor(props: any) {
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, no-console */
+export default function Editor(props: any): JSX.Element {
   const { value, onChange } = props;
 
-  function handleChange(editor: any, data: any, value: any) {
+  function handleChange(editor: unknown, data: unknown, value: string) {
+    console.log(editor);
+    console.log(data);
     onChange(value);
   }
   return (
