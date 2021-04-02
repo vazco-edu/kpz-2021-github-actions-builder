555d625f252a68c12b2cc1e9163aeb8948345828
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-02 19:50:03 +0200

    parsing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,63 @@
+import Ajv from 'ajv';
+import standaloneCode from 'ajv/dist/standalone';
+import jsyaml from 'js-yaml';
 import React, { useState } from 'react';
 
 import Editor from './Editor';
-function App() {
-  const [yaml, setYaml] = useState('');
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
+function App(): JSX.Element {
+  const [yaml, setYaml] = useLocalStorage('yaml', '');
+  const [click, setClick] = useState(true);
+  //tutaj prypisanie do zmiennej !!!!!!!!!!!!!
+  const man = yaml;
+  function parseYamltoJSON(text: string) {
+    // Parsing string to JSON
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const doc = jsyaml.load(text);
+    // eslint-disable-next-line no-console
+    console.log(text);
+    // eslint-disable-next-line no-console
+    console.log(doc);
+    // eslint-disable-next-line no-console
+    console.log(typeof doc);
+    // const ret = JSON.parse(doc); nie wiem czy tak, może(?)
+    // ^^^ - showing type of parsed string
+    return doc;
+  }
+
+  function validate(schema: Record<string, unknown>) {
+    const ajv = new Ajv({ code: { source: true } });
+
+    const valid = ajv.compile(schema);
+    // eslint-disable-next-line no-console
+    console.log(typeof valid);
+    const moduleCode = standaloneCode(ajv, valid);
+    // eslint-disable-next-line no-console
+    console.log(typeof moduleCode);
+  }
+  function handleClickEvent(event: any) {
+    setClick(!click);
+    if (click) {
+      // eslint-disable-next-line no-console
+      console.log(man);
+      // Tutaj chyba coś wywala, bo nie może wykonać JSON.parse (idk)
+      // const obj = JSON.parse(man);
+      // eslint-disable-next-line no-console
+      // console.log(obj);
+    }
+    setClick(!click);
+    const x = parseYamltoJSON(man);
+    validate(x);
+  }
   return (
     <>
       <div className="text-editor">
         <Editor value={yaml} onChange={setYaml} />
       </div>
-      <div className="result">ja</div>
-      <button className="PRESSME">KONWERTUJ</button>
+      <div className="result">{click ? yaml : ''}</div>
+      <button className="PRESSME" onClick={handleClickEvent}>
+        KONWERTUJ
+      </button>
     </>
   );
 }
