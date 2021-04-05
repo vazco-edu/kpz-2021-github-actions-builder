ea40182b9ef7d795b2471ff21cfa41c6d480805c
jmaczko <k.maczko8@gmail.com>
2021-04-05 12:46:07 +0200

    added schema comparison

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,23 @@ import standaloneCode from 'ajv/dist/standalone';
 import jsyaml from 'js-yaml';
 import React, { useState } from 'react';
 
+<<<<<<< HEAD
+=======
+import useLocalStorage from '../hooks/useLocalStorage';
+import { schema as schem } from '../schema/Schema';
+>>>>>>> a79a9dd (added a retarded schema comparison)
 import Editor from './Editor';
+
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
 function App(): JSX.Element {
   const [yaml, setYaml] = useLocalStorage('yaml', '');
-  const [click, setClick] = useState(true);
+  const [click, setClick] = useState(false);
   //tutaj prypisanie do zmiennej !!!!!!!!!!!!!
   const man = yaml;
   function parseYamltoJSON(text: string) {
     // Parsing string to JSON
     // eslint-disable-next-line @typescript-eslint/ban-types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const doc = jsyaml.load(text);
     // eslint-disable-next-line no-console
     console.log(text);
@@ -22,32 +29,46 @@ function App(): JSX.Element {
     console.log(typeof doc);
     // const ret = JSON.parse(doc); nie wiem czy tak, może(?)
     // ^^^ - showing type of parsed string
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return doc;
   }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function validate(data: any) {
+    const ajv = new Ajv();
 
-  function validate(schema: Record<string, unknown>) {
-    const ajv = new Ajv({ code: { source: true } });
-
-    const valid = ajv.compile(schema);
+    const validate = ajv.compile(schem);
+    const valid = validate(data);
+    if (!valid) {
+      // eslint-disable-next-line no-console
+      console.log(validate.errors);
+    }
     // eslint-disable-next-line no-console
-    console.log(typeof valid);
-    const moduleCode = standaloneCode(ajv, valid);
+    //console.log(typeof valid);
+    return valid;
+    //const moduleCode = standaloneCode(ajv, valid);
     // eslint-disable-next-line no-console
-    console.log(typeof moduleCode);
+    //console.log(typeof moduleCode);
   }
   function handleClickEvent(event: any) {
     setClick(!click);
+    // eslint-disable-next-line no-console
+    console.log(click);
     if (click) {
+      const x = parseYamltoJSON(man);
+      if (validate(x)) {
+        // eslint-disable-next-line no-console
+        console.log('valid');
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('mmm');
+      }
       // eslint-disable-next-line no-console
-      console.log(man);
+      //console.log(man);
       // Tutaj chyba coś wywala, bo nie może wykonać JSON.parse (idk)
       // const obj = JSON.parse(man);
       // eslint-disable-next-line no-console
       // console.log(obj);
     }
-    setClick(!click);
-    const x = parseYamltoJSON(man);
-    validate(x);
   }
   return (
     <>
