d5d8b9e91139195f4e5e3f143cf08e54595890f0
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-21 20:37:56 +0200

    JDD

--- src/diagrams/createDiagrams.tsx
+++ src/diagrams/createDiagrams.tsx
@@ -215,6 +215,7 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
                 continue;
               } else {
                 node.addOutPort(tt[properties]['branches'][j]);
+                console.log('JD');
               }
             }
           }
