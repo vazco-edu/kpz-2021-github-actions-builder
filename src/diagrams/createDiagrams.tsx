f2964d41da83540ee8f8eb2cda460e669c1e278f
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-25 13:15:08 +0200

    improved linking logic

--- src/diagrams/createDiagrams.tsx
+++ src/diagrams/createDiagrams.tsx
@@ -248,7 +248,7 @@ export default function createDiagrams(notNormalized: any, normalized: any) {
   const portsOutWithNeeds: DefaultPortModel[] = [];
   const portsIn: DefaultPortModel[] = [];
   // needs change - only displaying out ports of jobs that dont have needs
-  for (let j = 0; j < Object.keys(normalized['jobs']).length; ++j) {
+  for (let j = 0; j < numWithoutNeeds; ++j) {
     portsOut.push(node2.addOutPort((j + 1).toString()));
   }
 <<<<<<< HEAD
@@ -458,6 +458,7 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
 =======
   console.log(nodes[0]);
   console.log(nodes[1]);
+  console.log(nodes[2]);
   // console.log(portsIn);
   const link = port1.link<DefaultLinkModel>(port2);
   // console.log(portsOutWithNeeds);
@@ -465,8 +466,10 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
   const linksWithNeeds: DefaultLinkModel[] = [];
   //array storing links of jobs with needs
   const link2: DefaultPortModel[] = [];
+  let needsArr: any = [];
   for (let c = 0; c < portsIn.length; c++) {
     if (normalized['jobs'][`${Object.keys(normalized['jobs'])[c]}`].needs) {
+<<<<<<< HEAD
       console.log(nodes.length);
       for (
         let need = 0;
@@ -488,6 +491,51 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
             );
 >>>>>>> 3553cda (workin)
           }
+=======
+      // console.log(nodes.length);
+      // for (
+      //   let need = 0;
+      //   need <
+      //   normalized['jobs'][`${Object.keys(normalized['jobs'])[c]}`].needs
+      //     .length;
+      //   ++need
+      // ) {
+      for (let element = 1; element < portsIn.length; element++) {
+        console.log(nodes[element]['portsIn'][0]['options']['label']);
+        console.log(element);
+        if (nodes[element]['portsIn'][0]['options']['label']?.includes(',')) {
+          needsArr = nodes[element]['portsIn'][0]['options']['label']?.split(
+            ',',
+          );
+          console.log('dziele');
+        }
+        // console.log(needsArr);
+        // console.log(nodes[c - 1]['options']['name']);
+        // console.log(nodes[element]['portsIn'][0]['options']['label']);
+        if (needsArr.length) {
+          console.log('JD');
+          for (let need = 0; need < needsArr.length; ++need) {
+            console.log(need);
+            console.log(nodes[c - 1]['options']['name']);
+            console.log(needsArr[need]);
+            if (nodes[c - 1]['options']['name'] === needsArr[need]) {
+              console.log('IN');
+              linksWithNeeds.push(
+                portsOutWithNeeds[c - 1].link<DefaultLinkModel>(
+                  portsIn[element],
+                ),
+              );
+            }
+          }
+        } else if (
+          nodes[c - 1]['options']['name'] ===
+          nodes[element]['portsIn'][0]['options']['label']
+        ) {
+          console.log('WCHODZE');
+          linksWithNeeds.push(
+            portsOutWithNeeds[c - 1].link<DefaultLinkModel>(portsIn[element]),
+          );
+>>>>>>> fd7b4a4 (linkerr)
         }
 
         // console.log(
@@ -496,6 +544,12 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
         //   ],
         // );
       }
+
+      // console.log(
+      //   normalized['jobs'][`${Object.keys(normalized['jobs'])[c]}`].needs[
+      //     need
+      //   ],
+      // );
     } else {
       links.push(portsOut[c].link<DefaultLinkModel>(portsIn[c]));
     }
