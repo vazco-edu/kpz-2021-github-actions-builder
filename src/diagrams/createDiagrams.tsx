6ad2830eac17a028c3a5ff997726605adac30886
szym0nt <67016403+szym0nt@users.noreply.github.com>
2021-04-26 19:20:16 +0200

    changed loop boundary

--- src/diagrams/createDiagrams.tsx
+++ src/diagrams/createDiagrams.tsx
@@ -253,7 +253,7 @@ export default function createDiagrams(notNormalized: any, normalized: any) {
   const portsOutWithNeeds: DefaultPortModel[] = [];
   const portsIn: DefaultPortModel[] = [];
   // needs change - only displaying out ports of jobs that dont have needs
-  for (let j = 0; j < Object.keys(normalized['jobs']).length; ++j) {
+  for (let j = 0; j < numWithoutNeeds; ++j) {
     portsOut.push(node2.addOutPort((j + 1).toString()));
   }
 <<<<<<< HEAD
@@ -473,6 +473,8 @@ function helperPortCreation(normal: any, node: DefaultNodeModel): any {
   const link2: DefaultPortModel[] = [];
   let needsArr: any = [];
   let k = 0;
+  // eslint-disable-next-line prefer-const
+  let s = 0;
   for (let c = 0; c < portsIn.length; c++) {
     if (normalized['jobs'][`${Object.keys(normalized['jobs'])[c]}`].needs) {
 <<<<<<< HEAD
